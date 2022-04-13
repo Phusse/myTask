3110fa5a5ff96af8543b67103fb26c4f1077c653
Phusse <87185545+Phusse@users.noreply.github.com>
2022-04-13 14:06:26 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,33 @@
-﻿    {
-        Console.WriteLine("Enter the numbers YOu want and put a hyphen after each digit : ");
+    {
+        Console.WriteLine("Enter the numbers YOu want and put a comma after each digit : ");
         var name = Console.ReadLine();
-        var numarray = name.Split('-');
+        var numarray = name.Split(',');
+        Array.Sort(numarray);
         int firstDigit = Convert.ToInt32(numarray[0]);
-
+         
         bool cons = true;
+        
         for (var i = 0;i<numarray.Length;i++ )
+        
         {
             if (Convert.ToInt32(numarray[i])-i != firstDigit)
+            
             {
                 cons = false;                   
                 break;
             }
+            
         }
         if (cons)
+
         {
-            Console.WriteLine("True");
+            Console.WriteLine("True!");
         }
+
         else
-        {
-         
-            Console.WriteLine("False");
-        
+        {       
+            Console.WriteLine("False!");   
         }
+
     }
 
